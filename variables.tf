f09d2a90491d9b7601f863258338a143f51427ca
endormi <39559256+endormi@users.noreply.github.com>
2022-01-08 19:13:49 +0200

    Add variables.tf

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "name" {}
+variable "region" {}
+variable "vpc_cidr_block" {}
+variable "public_subnet1_cidr_block" {}
+variable "public_subnet2_cidr_block" {}
+variable "public_subnet3_cidr_block" {}
