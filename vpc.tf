258a8058886628cc034bb8a1968c26a42641d558
endormi <39559256+endormi@users.noreply.github.com>
2022-01-08 19:14:06 +0200

    Add provider and vpc

---
+++ vpc.tf
@@ -0,0 +1,59 @@
+resource "aws_vpc" "vpc" {
+  cidr_block       = "${var.vpc_cidr_block}"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "${var.name}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "${var.public_subnet1_cidr_block}"
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Name = "${var.name}-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "${var.public_subnet2_cidr_block}"
+  availability_zone = "${var.region}b"
+
+  tags = {
+    Name = "${var.name}-public-subnet-2"
+  }
+}
+
+resource "aws_subnet" "public-subnet-3" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "${var.public_subnet3_cidr_block}"
+  availability_zone = "${var.region}c"
+
+  tags = {
+    Name = "${var.name}-public-subnet-3"
+  }
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.name}-route-table"
+  }
+}
